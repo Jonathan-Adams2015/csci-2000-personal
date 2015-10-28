10ce669977f517416247fbb71f370ce8af9d45bf
Jonathan <Jonathan.Adams.777@gmail.com>
2015-10-28 10:30:35 -0400

    commited by Armin

---
+++ Midterm/has_no_e.py
@@ -0,0 +1,14 @@
+func has_no_e (string):
+    if string.count('e') == 0:
+        return true:
+    else :
+        return false:
+
+finde = open(gadsby_stripped.txt, r)
+while lines in gadsby_stripped.txt:
+    if has_no_e(variable.readline()):
+    else:
+        finde.close()
+        return false:
+finde.close()
+return true:
---
+++ Midterm/strip.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+#Jonathan Adams 100561416
+head -n -$2 $3 | tail -n +$1
