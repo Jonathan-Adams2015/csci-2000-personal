4a8cb1922bd546120f77ac0dc79e0b11a39d7221
Jonathan <Jonathan.Adams.777@gmail.com>
2015-11-07 03:59:28 -0500

    Assignment 2: completed has_no_e.py

---
+++ Assignments/Assignment-2/has_no_e.py
@@ -0,0 +1,13 @@
+def has_no_e(string):
+    if string.count('e') == 0:
+        return True
+    else:
+        return False
+
+find = open("gadsby_stripped.txt")
+for line in "gadsby_stripped.txt":
+    if not has_no_e(find.readline()):
+        find.close()
+        print("e is in file")
+find.close()
+print("e is not in file")
